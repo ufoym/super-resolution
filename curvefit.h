8e07ab62fc186ea4001b00fe896efbf86e20603e
Chi Zhang <chizhang.sysu@gmail.com>
2013-03-10 12:01:21 +0800

    add visualize polygon

--- curvefit.h
+++ curvefit.h
@@ -6,6 +6,8 @@
 #include "image.h"
 #include "helper.h"
 #include <Eigen/Dense>
+#include <iostream>
+#include <cmath>
 
 
 
@@ -34,18 +36,22 @@ public:
 				float minx = 99999, miny = minx;
 				float maxx = -1, maxy = maxx;
 
-				for (int i = idn - NUM_NEIGHBORS; i <= i + NUM_NEIGHBORS; i++) {
+				for (int i = idn - NUM_NEIGHBORS; i <= idn + NUM_NEIGHBORS; i++) {
 					// in the case shape_size is much smaller than NUM_NEIGHBORS, 
 					// i + shape_size may still be negative, 
 					// so we use shape_size * NUM_NEIGHBORS instead.
 					int idx = indices[ids][(i + shape_size * NUM_NEIGHBORS) % shape_size];
-					points.push_back(nodes[2 * idx]);
-					points.push_back(nodes[2 * idx + 1]);
+					int px = 2 * (i + NUM_NEIGHBORS - idn);
+					int py = px + 1;
+					points[px] = nodes[2 * idx];
+					points[py] = nodes[2 * idx + 1];
+					//points.push_back(nodes[2 * idx]);
+					//points.push_back(nodes[2 * idx + 1]);
 
-					minx = std::min(minx, points[2 * idx]);
-					maxx = std::max(maxx, points[2 * idx]);
-					miny = std::min(miny, points[2 * idx + 1]);
-					maxy = std::max(maxy, points[2 * idx + 1]);
+					minx = std::min(minx, points[px]);
+					maxx = std::max(maxx, points[px]);
+					miny = std::min(miny, points[py]);
+					maxy = std::max(maxy, points[py]);
 				}
 
 				ConicSection tmp;
@@ -57,23 +63,154 @@ public:
 				conics.push_back(tmp);
 			}
 		}
+		_saveConics();
 	}
 
+	void _saveConics()
+	{
+		std::ofstream file("conics.txt", std::ios::out);
+		for (int i = 0; i < conics.size(); i++) {
+			file << conics[i].A << "\t"
+				 << conics[i].B << "\t"
+				 << conics[i].C << "\t"
+				 << conics[i].D << "\t"
+				 << conics[i].E << "\t"
+				 << conics[i].F << std::endl;
+			file << "minx: " << conics[i].minx << std::endl
+				 << "maxx: " << conics[i].maxx << std::endl
+				 << "miny: " << conics[i].miny << std::endl
+				 << "maxy: " << conics[i].maxy << std::endl;
+		}
+		for (int i = 0; i < conics.size() ; i++) {
+			file << "ezplot('("
+				 << conics[i].A << ")*x*x + ("
+				 << conics[i].B << ")*x*y + ("
+				 << conics[i].C << ")*y*y + ("
+				 << conics[i].D << ")*x + ("
+				 << conics[i].E << ")*y + ("
+				 << conics[i].F << ") = 0')"
+				 << std::endl;
+		}
+
+		file.close();
+	}
 
 	void saveToSVG(std::string& filename)
 	{
 		std::ofstream file(filename, std::ios::out | std::ios::binary);
-		for (std::vector<ConicSection>::const_iterator it = conics.begin();
-			 it != conics.end(); it++) {
-				 // densely sample each conic section
-				 // and ouput the sampled points to file.
+		const std::string svg_header = "<?xml version=\"1.0\" standalone=\"no\"?>\n"
+			"<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"\n"
+			"\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n"
+			"<svg width=\"224\" height=\"225\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\">\n";
+		file << svg_header << std::endl;
+
+		std::ofstream file2("sample_size.txt", std::ios::out);
+
+		for (int i = 0; i < conics.size(); i++) {
+			// densely sample each conic section
+			// and ouput the sampled points to file.
+			std::vector<float> samples;
+			_sampleConicSection(conics[i], samples);
+			file2 << samples.size() << std::endl;
+
+			file << "<polyline points=\"";
+			for (int j = 0; j < samples.size(); j += 2) {
+				file << samples[j] << ","
+					 << samples[j + 1] << " ";
+			}
+			file << "\" style=\"fill-opacity:0; "
+				 << "stroke-opacity:0.8; "
+				 << "stroke:#00ff00; stroke-width:0.2\"/>"
+				 << std::endl;
 		}
+		file << "</svg>" << std::endl;
+		file.close();
+		file2.close();
+	}
+
+
+	void _sampleConicSection(ConicSection& conic, std::vector<float>& samples)
+	{
+		enum {FOREACH_X, FOREACH_Y};
+		int mode;
+		if ((conic.maxx - conic.minx) > (conic.maxy - conic.miny))
+			mode = FOREACH_X;
+		else 
+			mode = FOREACH_Y;
+
+		const double EPSILON = 0.1;
+		double a, b, c;
+		double A = conic.A, 
+			B = conic.B,
+			C = conic.C,
+			D = conic.D,
+			E = conic.E,
+			F = conic.F;
+			
+		if (mode == FOREACH_X) {
+			for (float x = conic.minx; x <= conic.maxx; x += 0.1f) {
+				a = C;
+				b = B*x + E;
+				c = A*x*x + D*x + F;
+				std::vector<float> y = _solveQuadratic(a, b, c);
+				for (int i = 0; i < y.size(); i++) {
+					if (conic.miny - EPSILON <= y[i] && y[i] <= conic.maxy + EPSILON) {
+						samples.push_back(x);
+						samples.push_back(y[i]);
+					}
+				}
+			}
+		}
+		else {
+			for (float y = conic.miny; y <= conic.maxy; y += 0.1f) {
+				a = A;
+				b = B*y + D;
+				c = C*y*y + E*y + F;
+				std::vector<float> x = _solveQuadratic(a, b, c);
+				for (int i = 0; i < x.size(); i++) {
+					if (conic.minx - EPSILON <= x[i] && x[i] <= conic.maxx + EPSILON) {
+						samples.push_back(x[i]);
+						samples.push_back(y);
+					}
+				}
+			}
+		}
+	}
+
+
+	std::vector<float> _solveQuadratic(double a, double b, double c)
+	{
+		const double eps = 1.0e-6;  // i wonder if there are some conventional epsilon.
+		std::vector<float> roots;
+
+		if (std::abs(a) < eps) {  // a == 0.
+			if (std::abs(b) > eps) {
+				roots.push_back(-c / b);
+			}
+			return roots;
+		}
+
+		double delta = b*b - 4*a*c;
+		if (delta < -eps) {  // delta < 0.
+			//std::cout << "zeros!!!" << std::endl;
+			return roots;
+		}
+
+		if (delta < eps) {  // treate it as 0.
+			roots.push_back(-b / (2*a));
+			return roots;
+		}
+
+		double sqrt_delta = std::sqrt(delta);
+		roots.push_back((-b - sqrt_delta) / (2*a));
+		roots.push_back((-b + sqrt_delta) / (2*a));
+		return roots;
 	}
 
 
 	void _fitConic(std::vector<float>& points, ConicSection& conic)
 	{
-		const int n = 4;//1 + 2 * NUM_NEIGHBORS;  // must equal to points.size() / 2.
+		const int n = 1 + 2 * NUM_NEIGHBORS;  // must equal to points.size() / 2.
 		Eigen::MatrixXd    b(n, 1);  // bi = -(yi)^2
 		Eigen::MatrixXd    A(n, 5);  // A  = [a1, a2, ..., an]^T
 									 // ai = [xi^2-yi^2, 2xiyi, 2xi, 2yi, 1]^T
@@ -102,14 +239,19 @@ public:
 		AT  = A.transpose();
 		ATA = AT * A;
 		ATb = AT * b;
-		p   = ATA.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
-
-		conic.A = p(0,0);
-		conic.B = p(1,0);
+		 p   = ATA.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(ATb);
+		//p = ATA.inverse() * ATb;
+#ifdef MY_DEBUG
+		std::cout << ATA << std::endl;
+		std::cout << ATb << std::endl;
+		std::cout << p	 << std::endl;
+#endif
+		conic.A = p(0, 0);
+		conic.B = p(1, 0) * 2;
 		conic.C = 1 - conic.A;
-		conic.D = p(2,0);
-		conic.E = p(3,0);
-		conic.F = p(4,0);
+		conic.D = p(2, 0) * 2;
+		conic.E = p(3, 0) * 2;
+		conic.F = p(4, 0);
 	}
 
 };
